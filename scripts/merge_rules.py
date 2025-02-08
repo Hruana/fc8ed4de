51a4899324d86bda996045c8139c87ecf9226214
Hruana <120722148+Hruana@users.noreply.github.com>
2025-02-08 12:27:45 +0800

    Add files via upload

--- scripts/merge_rules.py
+++ scripts/merge_rules.py
@@ -21,7 +21,8 @@ def download_rules(rule_urls: list):
         if response.status_code == 200:
             rules.extend(response.text.splitlines())
         else:
-            print(f"Failed to fetch rules from {url}")
+            logger.error(f'Failed to fetch rules from {url}')
+            exit()
     return rules
 
 
@@ -34,38 +35,42 @@ def classify_rules(rules):
 
     for rule in rules:
         rule = rule.strip()
-        if rule.startswith("DOMAIN-SUFFIX,"):
-            domain_suffix_rules.add(rule.split(",")[1])
-        elif rule.startswith("DOMAIN,"):
-            domain_rules.add(rule.split(",")[1])
-        elif rule.startswith("DOMAIN-KEYWORD,"):
-            domain_keyword_rules.add(rule.split(",")[1])
-        elif "IP-CIDR" in rule:
-            ip_cidr_rules.add(rule.split(",")[1].split(',')[0])
+        if rule.startswith('DOMAIN-SUFFIX,'):
+            domain_suffix_rules.add(rule.split(',')[1])
+        elif rule.startswith('DOMAIN,'):
+            domain_rules.add(rule.split(',')[1])
+        elif rule.startswith('DOMAIN-KEYWORD,'):
+            domain_keyword_rules.add(rule.split(',')[1])
+        elif 'IP-CIDR' in rule:
+            ip_cidr_rules.add(rule.split(',')[1].split(',')[0])
     return domain_rules, domain_suffix_rules, domain_keyword_rules, ip_cidr_rules
 
 
 # 输出规则
-def write_output(output_file, domain_rules, domain_suffix_rules, domain_keyword_rules, ip_cidr_rules):
+def write_output(output_file, domain_rules, domain_suffix_rules, domain_keyword_rules, ip_cidr_rules,
+                 additional_rules: list = []):
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
     # 取出output_file的文件名
     output_file_name = os.path.basename(output_file)
-    with open(output_file, "w") as f:
-        f.write(f"# NAME {output_file_name} \n")
-        f.write(
-            f"# Total: {len(domain_suffix_rules) + len(domain_rules) + len(domain_keyword_rules) + len(ip_cidr_rules)} Rules\n")
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.write(f'# NAME {output_file_name} \n')
+        total = len(domain_suffix_rules) + len(domain_rules) + len(domain_keyword_rules) + len(ip_cidr_rules) + len(
+            additional_rules) - 2
+        f.write(f'# Total: {total} Rules\n')
+        for rule in additional_rules:
+            f.write(f'{rule}\n')
         for rule in sorted(domain_suffix_rules):
-            f.write(f"DOMAIN-SUFFIX,{rule}\n")
+            f.write(f'DOMAIN-SUFFIX,{rule}\n')
         for rule in sorted(domain_rules):
-            f.write(f"DOMAIN,{rule}\n")
+            f.write(f'DOMAIN,{rule}\n')
         for rule in sorted(domain_keyword_rules):
-            f.write(f"DOMAIN-KEYWORD,{rule}\n")
+            f.write(f'DOMAIN-KEYWORD,{rule}\n')
         for cidr in sorted(ip_cidr_rules, key=lambda x: str(x)):
-            f.write(f"IP-CIDR,{cidr}\n")
-
+            f.write(f'IP-CIDR,{cidr}\n')
+    logger.info(f'Output {total} rules to {output_file}')
 
 # 主函数
-def merge(rule_urls: list, output_file: str):
+def merge(rule_urls: list, output_file: str, additional_rules: list = []):
     # 下载和分类规则
     rules = download_rules(rule_urls)
     domain_rules, domain_suffix_rules, domain_keyword_rules, ip_cidr_rules = classify_rules(rules)
@@ -74,7 +79,10 @@ def merge(rule_urls: list, output_file: str):
     optimized_cidrs = optimize_ip_cidr(ip_cidr_rules)
 
     # 输出优化后的规则
-    write_output(output_file, domain_rules, domain_suffix_rules, domain_keyword_rules, optimized_cidrs)
+    write_output(
+        output_file, domain_rules, domain_suffix_rules,
+        domain_keyword_rules, optimized_cidrs, additional_rules
+    )
 
 
 # 合并 NSFW 规则
@@ -86,8 +94,14 @@ def merge_nsfw_rules(output_file: str = '../rules/NSFW.list'):
         f'{GLOBAL_MIRROR}/devswork/my-subconverter/master/rules/porn.list',
         f'{GLOBAL_MIRROR}/sounfury/sounfury-Clash_rules/main/list/sexy.list',
     ]
-    merge(input_files, output_file)
-    logger.info(f"NSFW rules merged to {output_file}")
+    # 额外规则
+    additional_rules = [
+        '# 额外规则',
+        '# 合并规则(NSFW)',
+    ]
+    #
+    merge(input_files, output_file, additional_rules)
+    logger.info(f'NSFW rules merged to {output_file}')
 
 
 # 合并 LAN 规则
@@ -98,10 +112,36 @@ def merge_lan_rules(output_file: str = '../rules/LAN.list'):
         f'{GLOBAL_MIRROR}/GeQ1an/Rules/master/QuantumultX/Filter/LAN.list',
         f'{GLOBAL_MIRROR}/dler-io/Rules/main/Clash/Provider/LAN.yaml',
     ]
-    merge(input_files, output_file)
-    logger.info(f"LAN rules merged to {output_file}")
+    # 额外规则
+    additional_rules = [
+        '# 额外规则',
+        '# 合并规则(LAN)',
+    ]
+    #
+    merge(input_files, output_file, additional_rules)
+    logger.info(f'LAN rules merged to {output_file}')
 
 
-if __name__ == "__main__":
-    # merge_nsfw_rules()
+# 合并 Developer 规则
+def merge_developer_rules(output_file: str = '../rules/Developer.list'):
+    # 定义规则 URL
+    input_files = [
+        f'{GLOBAL_MIRROR}/blackmatrix7/ios_rule_script/master/rule/Clash/GitHub/GitHub.list',
+        f'{GLOBAL_MIRROR}/blackmatrix7/ios_rule_script/master/rule/Clash/Docker/Docker.list',
+        f'{GLOBAL_MIRROR}/blackmatrix7/ios_rule_script/master/rule/Clash/Python/Python.list',
+        f'{GLOBAL_MIRROR}/blackmatrix7/ios_rule_script/master/rule/Clash/Ubuntu/Ubuntu.list',
+    ]
+    # 额外规则
+    additional_rules = [
+        '# 额外规则',
+        '# 合并规则(GitHub, Docker, Python, Ubuntu)',
+    ]
+    #
+    merge(input_files, output_file, additional_rules)
+    logger.info(f'Developer rules merged to {output_file}')
+
+
+if __name__ == '__main__':
+    merge_nsfw_rules()
     merge_lan_rules()
+    merge_developer_rules()
